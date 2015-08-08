e5b1b65468197fed805cb35abbd8bffe768b51fa
Jason Felice <jason.m.felice@gmail.com>
2015-08-08 17:49:18 -0400

    Better test pattern

--- cube.asm
+++ cube.asm
@@ -4,14 +4,29 @@
 
 start:
 	jsr emit_layer
-	brk
+	jsr emit_bit_pause
+	jsr emit_bit_pause
+	jsr emit_bit_pause
+	jsr emit_bit_pause
+	jsr emit_bit_pause
+	jmp start
 
 output_port	equ $c010
 data_bit	equ $01
 shift_bit	equ $02
 light_bit	equ $04
 
-cube_memory	ds 64, $0f		; Bit-mapped "video memory"
+cube_memory	.byte $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff	; Bit-mapped "video memory"
+		.byte $f0, $f0, $f0, $f0, $f0, $f0, $f0, $f0
+		.byte $f0, $f0, $f0, $f0, $f0, $f0, $f0, $f0
+		.byte $f0, $f0, $f0, $f0, $f0, $f0, $f0, $f0
+		.byte $f0, $f0, $f0, $f0, $f0, $f0, $f0, $f0
+		.byte $f0, $f0, $f0, $f0, $f0, $f0, $f0, $f0
+		.byte $f0, $f0, $f0, $f0, $f0, $f0, $f0, $f0
+		.byte $00, $00, $00, $00, $00, $00, $00, $00
+
+current_layer	.byte $1
+current_offset	.byte $0
 
 emit_layer subroutine
 	sta .save_a
@@ -19,19 +34,31 @@ emit_layer subroutine
 	lda #0			; Turn off lights
 	sta output_port
 
-	ldx #0
+	ldx current_offset
+	txa
+	adc #8
+	sta current_offset
 .ebyte
 	lda cube_memory,x	; Send layer bytes (anodes)
 	jsr emit_byte
 	inx
-	cpx #8
+	cpx current_offset
 	bne .ebyte
 
-	lda #1			; Send layer enables (cathodes)
+	lda current_layer 	; Send layer enables (cathodes)
 	jsr emit_byte
 
 	lda #light_bit		; Light it back up
 	sta output_port
+
+	asl current_layer	; increment current layer
+	bcc .no_layer_wrap
+
+	lda #1			; reset to first layer
+	sta current_layer
+	lda #0
+	sta current_offset
+.no_layer_wrap
 
 	ldx .save_x
 	lda .save_a
